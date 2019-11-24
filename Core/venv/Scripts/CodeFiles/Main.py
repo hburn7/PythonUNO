2ba3d0fb191d141404efca2eeba6003c9fa57b47
StageOsu <stageosu@gmail.com>
2019-11-24 13:22:43 -0500

    Added multiplayer input at program start.

--- Core/venv/Scripts/CodeFiles/Main.py
+++ Core/venv/Scripts/CodeFiles/Main.py
@@ -1,4 +1,16 @@
 from Core.venv.Scripts.CodeFiles.Models import UnoCardModel
 
-uc = UnoCardModel.UnoCard('R', 6, False)  # Instantiation of the 'UnoCardModel' class.
+print('Welcome to Harry, Jordan, and Frankie\'s Python UNO!')
+
+# We use while True because if someone puts in a wrong input they will now be re-prompted.
+while True:
+    try:
+        numPlayers = int(input('How many players will be playing? Please enter a value between 2 and 8: '))
+        if 2 <= numPlayers <= 8:
+            break
+        print('Invalid number, please try again.')
+    except ValueError:
+        print("You have entered an invalid input. Please try again.")
+
+uc = UnoCardModel.UnoCard('R', 6, '')  # Instantiation of the 'UnoCardModel' class.
 print(uc.color, uc.number, uc.special)  # Returns "R 6 False"
--- Core/venv/Scripts/CodeFiles/Models/UnoCardModel.py
+++ Core/venv/Scripts/CodeFiles/Models/UnoCardModel.py
@@ -1,9 +1,30 @@
+from enum import Enum
+
+
 class UnoCard:
     color = ''  # R, G, B, Y
     number = 0  # Number 0-9 representing the card.
-    special = ''  # Here we can put something like D4 for Draw 4, CC for color change, S for skip.
+    special = ''  # R or S for reverse and skip respectively.
 
     def __init__(self, c, n, s):
         self.color = c
         self.number = n
         self.special = s
+
+
+class DrawCard:
+    amount = 0  # Amount of cards the player will draw when receiving this card (Will almost always be 2).
+    color = ''
+
+    def __init__(self, n, c):
+        self.amount = n
+        self.color = c
+
+
+# An Enum basically assigns a handy name to an integer. This way,
+# we can call the class and say OtherSpecial.DRAWFOUR instead of another
+# less easy-to-read naming scheme.
+
+class OtherSpecial(Enum):
+    COLORWHEEL = 1
+    DRAWFOUR = 2  # We need to account for the color change when someone draws 4.
