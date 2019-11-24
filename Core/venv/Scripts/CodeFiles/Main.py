ca681ef435732c07a5fa0c186069baf7861dd687
StageOsu <stageosu@gmail.com>
2019-11-24 12:53:47 -0500

    Update Main.py

--- Core/venv/Scripts/CodeFiles/Main.py
+++ Core/venv/Scripts/CodeFiles/Main.py
@@ -1,4 +1,4 @@
 from Core.venv.Scripts.CodeFiles.Models import UnoCardModel
 
 uc = UnoCardModel.UnoCard('R', 6, False)  # Instantiation of the 'UnoCardModel' class.
-print(uc.color, uc.number, uc.special) # Returns "R 6 False"
+print(uc.color, uc.number, uc.special)  # Returns "R 6 False"
