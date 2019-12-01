5f67ff21adf790f45d88129cc0c1827630190845
StageOsu <stageosu@gmail.com>
2019-12-01 14:34:46 -0500

    Joined separate files into one "Main" file - got rid of sep. files.

--- Core/venv/Scripts/CodeFiles/Main.py
+++ Core/venv/Scripts/CodeFiles/Main.py
@@ -1,22 +1,32 @@
-from Core.venv.Scripts.CodeFiles.Models import UnoCardModel
+# NOTE: 
 
-print('Welcome to Harry, Jordan, and Frankie\'s Python UNO!')
+class UnoCard:
+    color = ''  # R, G, B, Y
+    number = 0  # Number 0-9 representing the card.
+    special = ''  # R or S for reverse and skip respectively.
 
-# We use while True because if someone puts in a wrong input they will now be re-prompted.
-# while True:
-#     try:
-#         numPlayers = int(input('How many players will be playing? Please enter a value between 2 and 8: '))
-#         if 2 <= numPlayers <= 8:
-#             break
-#         print('Invalid number, please try again.')
-#     except ValueError:
-#         print("You have entered an invalid input. Please try again.")
+    def __init__(self, color, num, special):
+        self.color = color
+        self.number = num
+        self.special = special
 
-# ** I COMMENTED THE ABOVE SO THAT WE DON'T HAVE TO TYPE IT IN EVERY TIME WE WANNA DEBUG! :D **
+class UnoCardSpecial:
+    color = '' # R, G, B, Y
+    special = '' # D4W, D2, W, R, S
 
-uc = UnoCardModel.UnoCard('R', 6, '')  # Instantiation of the 'UnoCardModel' class.
-print(uc.color, uc.number, uc.special)  # Returns "R 6 False"
+    def __init__(self, color, specialModifier):
+        self.color = color
+        self.special = specialModifier
 
-special = UnoCardModel.OtherSpecial('D4')  # Draw 4.
+class Main:
+    print('Welcome to Harry, Jordan, and Frankie\'s Python UNO!')
 
-print(special.type)
+    # We use while True because if someone puts in a wrong input they will now be re-prompted.
+    while True:
+        try:
+            numPlayers = int(input('How many players will be playing? Please enter a value between 2 and 8: '))
+            if 2 <= numPlayers <= 8:
+                break
+            print('Invalid number, please try again.')
+        except ValueError:
+            print("You have entered an invalid input. Please try again.")
