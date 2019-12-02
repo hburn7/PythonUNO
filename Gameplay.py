f6512a195b23664ad3ea249caf53cbe5bd1d5033
stageosu <stageosu@gmail.com>
2019-12-02 18:43:24 -0500

    Lot's o progress!

--- Gameplay.py
+++ Gameplay.py
@@ -1,56 +1,119 @@
-import UnoCardModel
-import GenerateDeck
-import random
+def UNO():  
+    import random  
+    deck = []
+    discard = []
+    colors = ['Red','Blue','Green','Yellow']
+    special = ['Draw 2','Skip','Reverse']
 
-from random import shuffle
-from itertools import product, repeat, chain
-    
-def create_uno_deck():
-   COLORS = ['red', 'yellow', 'green', 'blue']
-   NUMBERS = list(range(10)) + list(range(1, 10))
-   COLORED_SPECIALS = ['skip', 'reverse', '+2']
-   COLOR_CARD_TYPES = NUMBERS + COLORED_SPECIALS * 2
-   WILD_CARD_TYPES = ['colorswap', '+4']
-   color_cards = product(COLORS, COLOR_CARD_TYPES)
-   wild_cards = product(repeat('wild', 4), WILD_CARD_TYPES)
-   deck = list(chain(color_cards, wild_cards))
-   shuffle(deck)
-   return deck
+    for h in range(2):
+        for i in colors:
+            for j in range(1,10):
+                card = i + ' ' + str(j)
+                deck.append(card)
 
+    for i in range(1):
+        for j in colors:
+            card = j + ' ' + str(0)
+            deck.append(card)
 
-def deal_one_card(p,deck):
-    if len(deck)==0:
-        return
-    i = random.randint(0,len(deck)-1)
-    p.cards.append(deck[i])
-    deck.remove(deck[i])
-def create_discard_pile(deck):
-    discard_pile = []
-    i = random.randint(0,len(deck)-1)
-    discard_pile.append(deck[i])
-    deck.remove(deck[i])
-    return discard_pile
-def deal(p1,p2,p3,p4,deck):
+    for i in range(2):
+        for j in colors:
+            for k in special:
+                card = j + ' ' + k
+                deck.append(card)
+
+    for i in range(4):
+        card = 'Wild'
+        deck.append(card)
+        card = 'Wild Draw 4'
+        deck.append(card)
+
+    #print(len(deck))
+
+    while True:
+        try:
+            numPlayers = int(input('How many players will be playing? Please enter a value between 2 and 8: '))
+            if 2 <= numPlayers <= 8:
+                break
+            print('Invalid number, please try again.')
+        except ValueError:
+            print("You have entered an invalid input. Please try again.")
+
+    player_hands = {}
+
+    for i in range(1,(numPlayers + 1)):
+        player_hands['Player' + str(i)] = []
+    #print(player_hands)
+
+    random.shuffle(deck)
+
+    # print(deck)
+
     for i in range(7):
-        deal_one_card(p1,deck)
-        deal_one_card(p2,deck)
-        deal_one_card(p3,deck)
-        deal_one_card(p4,deck)
-        deal_one_card(p5,deck)
-        deal_one_card(p6,deck)
-        deal_one_card(p7,deck)
-        deal_one_card(p8,deck)
-            
+        for j in range(1,len(player_hands) + 1):
+            player_hands['Player' + str(j)].append(deck[0])
+            del deck[0]
+    
+    # print(player_hands)
+    # print(len(deck))
+    
+    count = 1
+    
+    discard.append(deck[0])
+    deck.remove(deck[0])
+    print(discard[0])
 
-p1 = Player("Marcus",1)
-p2 = Player("Harry",2)
-p3 = Player("Jordan",3)
-p4 = Player("Franklin",4)
-deck = create_uno_deck()
-deal(p1,p2,p3,p4,deck)
-discard_pile = create_discard_pile(deck)
-print(p1)
-print(p2)
-print(p3)
-print(p4)
-print(discard_pile[-1])
+    while count <= (len(player_hands)):
+        print('Player' + str(count) + "'s hand: ", player_hands['Player' + str(count)])
+        index_of_card = ((int(input('Player %s, what number card would you like to play? ' % str(count)))) - 1)
+        
+        player_hand = player_hands['Player' + str(count)]
+        card = player_hand[index_of_card]
+
+        PlayCard(card, discard)
+
+        if(CardIsNotMatch(discard, card)):
+            DrawCard(deck, player_hand) # MAKE THEM DRAW HERE
+
+        if len(player_hands['Player' + str(count)]) == 0:
+            print('Player' + str(count) + ' wins!')
+            count = 9
+        count = count + 1
+        if count == (len(player_hands) + 1):
+           count = 1
+
+def PlayCard(card, discard):
+    if(CardIsMatch(discard, card)):
+        discard.append(card)
+        print('Discard Pile: ', discard[-1])
+        del card
+    else:
+        print('Hey, did you forget the rules?! This card is not playable. Please try again.')
+
+
+def CardIsNotWild(card):
+    if(card[0] != 'W'):
+        return True
+    return False
+
+def CardIsWild(card):
+    if(card[0] == 'W'):
+        return True
+    return False
+
+def CardIsNotMatch(discard, card):
+    if(discard[-1][0] != card[0] and
+        discard[-1][1] != card[1] and CardIsNotWild(card)):
+        return True
+    return False
+
+def CardIsMatch(discard, card):
+    if(discard[-1][0] == card[0] or
+        discard[-1][1] == card[1] or CardIsWild(card)):
+        return True
+    return False
+
+def DrawCard(deck, player_hand):
+    return player_hand.append(deck[0])
+
+UNO()
--- Main.py
+++ Main.py
@@ -1,24 +1,6 @@
 # NOTE: All classes must be above "Main" class.
 
-import UnoCardModel
+import Gameplay
 
 class Main:
-    print('Welcome to Harry, Jordan, and Frankie\'s Python UNO!')
-
-    # We use while True because if someone puts in a wrong input they will now be re-prompted.
-    while True:
-        try:
-            numPlayers = int(input('How many players will be playing? Please enter a value between 2 and 8: '))
-            if 2 <= numPlayers <= 8:
-                break
-            print('Invalid number, please try again.')
-        except ValueError:
-            print("You have entered an invalid input. Please try again.")
-
-
-
-
-    # count = 1
-    
-    # while count < (len(num_players) + 1):
-    #     index_of_card = ((int(input('Player '+ str(count) + 'What number card would you like to play? '))) + 1):
+    Gameplay.Gameplay.UNO()
